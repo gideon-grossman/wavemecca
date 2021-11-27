12c26c54fc02677877e616cc886e2fb6ee904634
gideongrossmanHome <gideongrossman@gmail.com>
2021-11-27 20:46:21 +0100

    Big design upgrade
    Also adds dummy data file for testing without hitting IG API and a loading spinner of a surfer svg that is pretty neat.

---
+++ src/components/SearchResults.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <div v-for="edge in searchResults" :key="edge.display_url"
+>
+      <div
+        v-if="taggedWithSurfingKeywords(edge.node.edge_media_to_caption.edges)"
+      >
+        <img
+         @mouseover="displayCaptions(edge)"
+          width="300px"
+          :src="getImageSrc(edge.imageBase64)"
+          :alt="getImageSrc(edge.imageBase64)"
+        />
+        <div
+          v-for="textedge in edge.node.edge_media_to_caption.edges"
+          :key="textedge.node.text"
+        >
+          <p>{{ textedge.node.text }}</p>
+        </div>
+        <!-- <p>{{ edge.node.taken_at_timestamp }}</p> -->
+        <!-- <p>{{ getDateTime(edge.node.taken_at_timestamp) }}</p> -->
+        <p>
+          <b>{{ new Date(edge.node.taken_at_timestamp * 1000) }}</b>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchResults",
+  data() {
+    return {};
+  },
+  props: ["searchResults"],
+  methods: {
+    taggedWithSurfingKeywords(textedges) {
+      var surfingKeywords = ["tamraght", "surf", "ocean", "kite", "board"];
+
+      for (var i = 0; i < textedges.length; i++) {
+        // if (textedges[i].node.text.includes("surf")) {
+        if (surfingKeywords.some((el) => textedges[i].node.text.includes(el))) {
+          return true;
+        }
+      }
+      return false;
+    },
+    getImageSrc(base64Image) {
+      return "data:image/png;base64 " + base64Image;
+    },
+    displayCaptions(edge) {
+        console.log(Object.keys(edge.node));
+        // this.showToolTip();
+    }
+  },
+};
+</script>
+
+<style scoped>
+</style>
